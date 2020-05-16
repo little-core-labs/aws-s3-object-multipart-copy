f2c6184078fe1e95e7838c61003e290632f2aff2
Joseph Werle <joseph.werle@gmail.com>
2020-05-16 17:35:04 -0400

    test(test.js): simple construct - tests are TODO

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const { Destination, Session, Source } = require('./')
+const { S3 } = require('aws-sdk')
 const copy = require('./')
 const test = require('tape')
-test('simple', (t) => {
+
+test('session', (t) => {
+  const s3 = new S3()
+  const session = new Session({ s3 })
   t.end()
 })
